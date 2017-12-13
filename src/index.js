cd70320da8746b88599722baa5e77404e437e3d7
BlazingFire007 <elijahrich02@icloud.com>
2017-12-13 13:21:37 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const parser = require("./parse/parser.js")(client);
 
-client.on("message", parse.parse);
+client.on("message", parser.parse);
 
 
 client.login("");
