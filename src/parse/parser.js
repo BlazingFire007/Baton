32e1eeaf10821ada9c03f9b708f85bc37012b4d8
BlazingFire007 <elijahrich02@icloud.com>
2017-12-13 13:22:33 -0600

    Update parser.js

--- src/parse/parser.js
+++ src/parse/parser.js
@@ -2,4 +2,7 @@ module.exports = class Parser() {
   constructor(client) {
     this.client = client;
   }
+  parse(message) {
+    if (message.author.bot || message.channel.type !== "text") return;
+  }
 };
